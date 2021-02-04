9a88210577dca6fdbafd158a1b77ede3a4c513cb
mattia <slinucs@gmail.com>
2021-02-04 16:53:08 +0100

    new xpath links, EpicGame account for login, PEP8

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import pyotp
 import time
 import traceback
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException
+from selenium.common.exceptions import TimeoutException, \
+    NoSuchElementException, WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -41,7 +42,7 @@ def open_browser():
     chrome_driver_path = '/usr/bin/chromedriver'
 
     if DEBUG:
-        #chrome_driver_path = './chromedriver'
+        chrome_driver_path = './chromedriver'
         # window size when run in headless mode.
         # this is necessary as some styles are dynamic
         chrome_options.add_argument('--window-size=800,600')
@@ -55,7 +56,10 @@ def purchase_steps(browser):
     # wait until its visible and then click the purchase button
     logger.debug('find and click on the last purchase button')
     WebDriverWait(browser, TIMEOUT).until(
-        EC.visibility_of_element_located((By.XPATH, "//button[contains(@class,'btn-primary')]"))
+        EC.visibility_of_element_located((
+            By.XPATH,
+            "//button[contains(@class,'btn-primary')]"
+        ))
     ).click()
 
     # wait until its visible and then click the 'I Agree" popup
@@ -63,19 +67,25 @@ def purchase_steps(browser):
     try:
         logger.debug('accept the conditions of refund popup')
         WebDriverWait(browser, TIMEOUT).until(
-            EC.visibility_of_all_elements_located(
-                (By.XPATH, "//button[contains(@class,'btn-primary')]"))
+            EC.visibility_of_all_elements_located((
+                By.XPATH,
+                "//button[contains(@class,'btn-primary')]"
+            ))
         )[1].click()
-    except (NoSuchElementException, TimeoutException, LookupError) as ex:
+
+    except (NoSuchElementException, TimeoutException, LookupError):
         logger.debug('no refund conditions popup to accept')
 
     logger.debug('Wait for confirmation that checkout is complete')
 
     # Purchase should be complete. Checking for confirmation
-    WebDriverWait(browser, TIMEOUT).until(EC.visibility_of_element_located((
-        By.XPATH,
-        "//h1/span[contains(text(),'Install')]|//span[contains(text(),'Thank you for buying')]"
-    )))
+    WebDriverWait(browser, TIMEOUT).until(
+        EC.visibility_of_element_located((
+            By.XPATH,
+            "//h1/span[contains(text(),'Install')]|"
+            "//span[contains(text(),'Thank you for buying')]"
+        ))
+    )
 
 
 def execute():
@@ -85,24 +95,40 @@ def execute():
     try:
         logger.debug('find and click on login button')
         el = WebDriverWait(browser, TIMEOUT).until(
-            EC.element_to_be_clickable((By.XPATH, "//div[@id='user']/ul/li/a/span"))
+            EC.element_to_be_clickable((
+                By.XPATH,
+                "//div[@id='user']/ul/li/a/span"
+            ))
         )
         el.click()
 
+        logger.debug('find and click on EpicGame login method')
         el = WebDriverWait(browser, TIMEOUT).until(
-            EC.element_to_be_clickable((By.XPATH, "//div[@id='login-with-epic']/div[2]/span/h6"))
+            EC.element_to_be_clickable((
+                By.XPATH,
+                "//div[@id='login-with-epic']/div[2]/span/h6"
+            ))
         )
         el.click()
 
         logger.debug('wait for email field on login page')
         el = WebDriverWait(browser, TIMEOUT).until(
-            EC.element_to_be_clickable((By.XPATH, "//input[@id='email']"))
+            EC.element_to_be_clickable((
+                By.XPATH,
+                "//input[@id='email']"
+            ))
         )
 
         el.send_keys(EMAIL)
-        browser.find_element_by_xpath("//input[@id='password']").send_keys(PASSWORD)
+        browser.find_element_by_xpath(
+            "//input[@id='password']"
+        ).send_keys(PASSWORD)
+
         WebDriverWait(browser, TIMEOUT).until(
-            EC.element_to_be_clickable((By.XPATH, "//button[@id='sign-in']"))
+            EC.element_to_be_clickable((
+                By.XPATH,
+                "//button[@id='sign-in']"
+            ))
         ).click()
 
         try:
@@ -110,8 +136,9 @@ def execute():
             WebDriverWait(browser, TIMEOUT).until(
                 EC.presence_of_element_located((
                     By.XPATH,
-                    '//iframe[@title="arkose-enforcement"]'
-                )))
+                    '//iframe[@id="talon_frame_login_prod"]'
+                ))
+            )
             logger.critical('Captcha found. Can\'t procede any further.')
             return
         except TimeoutException:
@@ -129,10 +156,17 @@ def execute():
         try:
             # confirm login
             logger.debug('search for wrong credentials message')
-            WebDriverWait(browser, TIMEOUT).until(EC.visibility_of_element_located((
-                By.XPATH, "//h6[contains(text(),'credentials') and contains(text(),'invalid')]"
-            )))
-            logger.critical('failed to login into account, credentials invalid')
+            WebDriverWait(browser, TIMEOUT).until(
+                EC.visibility_of_element_located((
+                    By.XPATH,
+                    "//h6[contains(text(),'credentials') "
+                    "and contains(text(),'invalid')]"
+                ))
+            )
+
+            logger.critical(
+                'failed to login into account, credentials invalid'
+            )
             return
         except TimeoutException:
             logger.debug('login succeeded')
@@ -142,7 +176,10 @@ def execute():
             # get free games available
             logger.debug('wait for and get all free games available')
             games_found = WebDriverWait(browser, TIMEOUT).until(
-                EC.visibility_of_all_elements_located((By.XPATH, "//a[descendant::span[text()='Free Now']]"))
+                EC.visibility_of_all_elements_located((
+                    By.XPATH,
+                    "//a[descendant::span[text()='Free Now']]"
+                ))
             )
         except TimeoutException:
             logger.critical('no free games found')
@@ -150,15 +187,22 @@ def execute():
 
         try:
             logger.debug('close the cookies banner')
-            # close cookie policy span as it interferes with clicking on the purchase button
-            browser.find_element_by_xpath("//button[@id='euCookieAccept']").click()
+            # close cookie policy span as it interferes
+            # with clicking on the purchase button
+            browser.find_element_by_xpath(
+                "//button[@id='onetrust-accept-btn-handler']"
+            ).click()
+
         except NoSuchElementException:
             logger.debug('no cookies banner to close')
 
         for i in range(len(games_found)):
             logger.debug('wait for and get all free games available')
             games_found = WebDriverWait(browser, TIMEOUT).until(
-                EC.visibility_of_all_elements_located((By.XPATH, "//a[descendant::span[text()='Free Now']]"))
+                EC.visibility_of_all_elements_located((
+                    By.XPATH,
+                    "//a[descendant::span[text()='Free Now']]"
+                ))
             )
 
             games_found[i].click()
@@ -167,20 +211,38 @@ def execute():
             logger.debug('bypass mature content block')
             try:
                 WebDriverWait(browser, TIMEOUT).until(
-                    EC.visibility_of_element_located((By.XPATH, "//p[contains(text(),'mature content')]")))
+                    EC.visibility_of_element_located((
+                        By.XPATH,
+                        "//p[contains(text(),'mature content')]"
+                    ))
+                )
+
                 WebDriverWait(browser, TIMEOUT).until(
-                    EC.visibility_of_element_located((By.XPATH, "//span[contains(text(),'Continue')]"))).click()
+                    EC.visibility_of_element_located((
+                        By.XPATH,
+                        "//span[contains(text(),'Continue')]"
+                    ))
+                ).click()
+
             except TimeoutException:
                 logger.debug('no mature content block to bypass')
 
             logger.debug('find the purchase button')
             purchase_button = WebDriverWait(browser, TIMEOUT).until(
-                EC.visibility_of_element_located((By.XPATH, "//button[contains(@class,'Purchase')]"))
+                EC.visibility_of_element_located((
+                    By.XPATH,
+                    "//div[@id='dieselReactWrapper']/div/div[4]/main/div"
+                    "/div[3]/div[2]/div/div[2]/div[2]/div/div/div[3]"
+                    "/div/div/div/div[3]/div/div/button/span"
+                ))
             )
 
             # name of the game
             logger.debug('extract game title')
-            name = browser.find_element_by_xpath("//h1[contains(@class,'NavigationVertical')]").text
+            name = browser.find_element_by_xpath(
+                "//div[@id='dieselReactWrapper']/div/div[4]/"
+                "main/div/div/div/nav/div/div/ul/li[2]"
+            ).text
 
             # price formatted as '£11.99'
             logger.debug('extract game price')
@@ -188,51 +250,78 @@ def execute():
 
             # date formatted as 'Sale ends 11/29/2019 at 3:59 PM'
             logger.debug('extract sales end date')
-            expires = browser.find_element_by_xpath("//span[contains(text(),'Sale ends')]").text
+            expires = browser.find_element_by_xpath(
+                "//span[contains(text(),'Sale ends')]"
+            ).text
 
             if purchase_button.text == 'OWNED':
-                logger.info('\"%s\" already owned. Price was %s and %s', name, price, expires)
+                logger.info(
+                    '\"%s\" already owned. Price was %s and %s',
+                    name, price, expires
+                )
+
             elif purchase_button.text == 'GET':
                 logger.info('obtaining \"%s\"', name)
                 purchase_button.click()
 
                 purchase_steps(browser)
 
-                logger.info('obtained %s. Price was %s and %s', name, price, expires)
+                logger.info(
+                    'obtained %s. Price was %s and %s',
+                    name, price, expires
+                )
+
             elif purchase_button.text == 'SEE EDITIONS':
                 logger.debug('processing editions for \"%s\"', name)
                 # used to know the number of elements and titles
                 # without having to query on every refresh
                 editions_addons_titles = WebDriverWait(browser, TIMEOUT).until(
                     EC.visibility_of_all_elements_located((
-                        By.XPATH, "//div[contains(@class,'Editions-title') or contains(@class, 'AddOns-title')]"
-                    )))
-                editions_addons_titles = [i.text for i in editions_addons_titles]
+                        By.XPATH, "//div[contains(@class,'Editions-title') "
+                                  "or contains(@class, 'AddOns-title')]"
+                    ))
+                )
+
+                editions_addons_titles = [i.text for i in
+                                          editions_addons_titles]
 
                 for t in range(len(editions_addons_titles)):
-                    editions_addons_buttons = WebDriverWait(browser, TIMEOUT).until(
+                    editions_addons_buttons = WebDriverWait(browser, TIMEOUT)
+
+                    editions_addons_buttons.until(
                         EC.visibility_of_all_elements_located((
                             By.XPATH,
-                            "//div[contains(@class,'Editions') or contains(@class, 'AddOns')]//div[contains(@class,'PurchaseButton-ctaButtons')]//button"
-                        )))
+                            "//div[contains(@class,'Editions') or contains("
+                            "@class, 'AddOns')]//div[contains("
+                            "@class,'PurchaseButton-ctaButtons')]//button"
+                        ))
+                    )
 
                     if editions_addons_buttons[t].text == 'OWNED':
-                        logger.info('\"%s - %s\" already owned', name, editions_addons_titles[t])
+                        logger.info(
+                            '\"%s - %s\" already owned',
+                            name, editions_addons_titles[t]
+                        )
+
                     elif editions_addons_buttons[t].text == 'GET':
                         editions_addons_buttons[t].click()
 
                         purchase_steps(browser)
 
-                        logger.info('obtained \"%s - %s\"', name, editions_addons_titles[t])
+                        logger.info('obtained \"%s - %s\"',
+                                    name, editions_addons_titles[t])
 
                         browser.execute_script("window.history.go(-1)")
             else:
-                logger.warning('purchase button text not recognized: %s', purchase_button.text)
+                logger.warning(
+                    'purchase button text not recognized: %s',
+                    purchase_button.text
+                )
 
             # navigate back to free games page
             browser.get(FREE_GAMES_PAGE_URL)
         logger.info('all games processed')
-    except (TimeoutException, NoSuchElementException, WebDriverException) as ex:
+    except (TimeoutException, NoSuchElementException, WebDriverException):
         logger.critical(traceback.format_exc())
     browser.get('https://www.epicgames.com/logout')
     browser.close()
